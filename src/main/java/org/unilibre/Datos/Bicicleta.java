42de894308250cd8a407c7d1519ee1dd23929843
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2026-09-14 08:34:47 -0500

    Se agrego la enum, y las clases se actualizaron

--- src/main/java/org/unilibre/Datos/Bicicleta.java
+++ src/main/java/org/unilibre/Datos/Bicicleta.java
@@ -1,4 +1,33 @@
 package org.unilibre.Datos;
 
+import java.io.Serial;
+
 public class Bicicleta {
+    private int idPropietario;
+    private String serial;
+    private String color;
+
+    public void modificarIdpropietario(int idPropietario){
+    }
+
+    public int obtenerIdpropietario(){
+        return idPropietario;
+    }
+
+    public void modificarSerial(int serial){
+    }
+
+    public String obtenerSerial(){
+        return serial;
+    }
+
+    public void modificarColor(String color){
+    }
+
+    public String obtenerColor(){
+        return color;
+    }
+
+
+
 }
--- src/main/java/org/unilibre/Datos/RegistroEstacionamiento.java
+++ src/main/java/org/unilibre/Datos/RegistroEstacionamiento.java
@@ -1,4 +1,16 @@
 package org.unilibre.Datos;
 
+import java.util.ArrayList;
+
 public class RegistroEstacionamiento {
+    private ArrayList<Bicicleta> bicicletas;
+
+    public void modificarBicicletas(ArrayList<Bicicleta> bicicletas){
+    }
+
+    public ArrayList<Bicicleta> Obtenerbicicletas(){
+        return bicicletas;
+    }
+
+
 }
