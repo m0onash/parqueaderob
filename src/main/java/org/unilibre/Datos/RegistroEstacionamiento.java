7c3e48ec880d808a257b80021430f3316b20e448
Romer <romersasito@gmail.com>
2026-09-20 15:31:43 -0500

    se actualizo un error en el precio

--- src/main/java/org/unilibre/Datos/RegistroEstacionamiento.java
+++ src/main/java/org/unilibre/Datos/RegistroEstacionamiento.java
@@ -6,7 +6,6 @@ import java.util.Random;
 public class RegistroEstacionamiento {
     private ArrayList<Bicicleta> bicicletas;
 
-    // Variables para almacenar temporalmente los datos del último cobro realizado
     private int ultimoTiempo;
     private int ultimoCosto;
 
@@ -23,8 +22,6 @@ public class RegistroEstacionamiento {
     }
 
     public int calcularTiempoMinutos() {
-        // Como no tenemos una "hora de entrada" registrada en Bicicleta,
-        // simularemos un tiempo aleatorio entre 30 y 240 minutos para el ejercicio.
         Random random = new Random();
         this.ultimoTiempo = random.nextInt(210) + 30; // Minutos simulados
 
@@ -33,7 +30,7 @@ public class RegistroEstacionamiento {
 
     public int calcularCostoTotal() {
         // Supongamos que el costo es de $50 pesos por cada minuto
-        int tarifaPorMinuto = 50;
+        int tarifaPorMinuto = 10;
         this.ultimoCosto = this.ultimoTiempo * tarifaPorMinuto;
 
         return this.ultimoCosto;
@@ -43,7 +40,7 @@ public class RegistroEstacionamiento {
         StringBuilder recibo = new StringBuilder();
         recibo.append("========== TICKET DE SALIDA ==========\n");
         recibo.append("Tiempo de parqueo : ").append(ultimoTiempo).append(" minutos\n");
-        recibo.append("Tarifa por minuto : $ 50\n");
+        recibo.append("Tarifa por minuto : $ 10\n");
         recibo.append("--------------------------------------\n");
         recibo.append("TOTAL A PAGAR     : $ ").append(ultimoCosto).append("\n");
         recibo.append("======================================\n");
